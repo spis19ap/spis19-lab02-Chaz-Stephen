1f69cd2ff48a1cad15e7c4a9ebc8be81edc6ec4a
spis19ap <spis19ap@its-cseb240-39.ucsd.edu>
2019-08-08 13:21:48 -0700

    edited while loop and removed lines of code that were not needed

--- testSum.py
+++ testSum.py
@@ -1,8 +1,16 @@
 # The goal of this program is to practice Python constructs
-def sumTwo(a,b):
-   c = a + b
-   return c
+def getNumber():
+   #empty string to add numbers
+    allSymbols = ""
+    #while loop to repeat the digit prompt
+   while True:
+     symbols = input("Enter a digit: ")
+     #in the case a user enters a negative digit, the operation breaks
+     if int(symbols) < 0:
+         break
+     allSymbols += symbols
+   #once the operation is broken, this code presents the user with all the digits they entered prior as one number 		
+   number = int(allSymbols)
+   return number
 
-x = sumTwo(3,5)
-print(x)
-
+print (getNumber())
