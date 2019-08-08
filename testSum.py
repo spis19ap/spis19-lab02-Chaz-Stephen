5a2d73f742984298852ea4a579d25339db03fdcc
spis19ap <spis19ap@its-cseb240-39.ucsd.edu>
2019-08-08 13:48:35 -0700

    added sumDigits function

--- testSum.py
+++ testSum.py
@@ -1,16 +1,28 @@
 # The goal of this program is to practice Python constructs
 def getNumber():
-   #empty string to add numbers
-    allSymbols = ""
-    #while loop to repeat the digit prompt
+#empty string to add numbers
+   allSymbols = ""
+#while loop to repeat the digit prompt
    while True:
      symbols = input("Enter a digit: ")
-     #in the case a user enters a negative digit, the operation breaks
+#in the case a user enters a negative digit, the operation breaks
      if int(symbols) < 0:
          break
      allSymbols += symbols
-   #once the operation is broken, this code presents the user with all the digits they entered prior as one number 		
+#once the operation is broken, this code presents the user with all the digits they entered prior as one number 		
    number = int(allSymbols)
    return number
 
 print (getNumber())
+
+#this code here gets the digits from the program above and adds the digits
+def sumDigits (x):
+#variables with equations for getting each individual digit
+    ones = x%10
+    tens = x//10%10
+    hundreds = x//100
+#equation to add each of these individual digits together    
+    sum = ones + tens + hundreds
+    return sum
+
+print (sumDigits(getNumber()))
